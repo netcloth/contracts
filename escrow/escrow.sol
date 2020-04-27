8825b1fb394cc441fbe613815ff37cebab3bf273
iavl <z6521256@gmail.com>
2020-04-27 19:16:05 +0800

    add escrow contract

---
+++ escrow/escrow.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.6.0;
+
+import "../math/SafeMath.sol";
+import "../Ownable.sol";
+import "../utils/Address.sol";
+
+contract Escrow is Ownable{
+    using SafeMath for uint256;
+    using Address for address payable;
+
+    event Deposited(address indexed payee, uint256 amount);
+    event Withdrawn(address indexed payee, uint256 amount);
+
+    mapping (address => uint256) private _deposits;
+
+    function depositOf(address payee) public view returns (uint256) {
+        return _deposits[payee];
+    }
+
+    /**
+     * @dev Stores the sent amount as credit to be withdrawn.
+     * @param payee The destination address of the funds.
+     */
+    function deposit(address payee) public virtual payable onlyOwner {
+        uint256 amount = msg.value;
+        _deposits[payee] = _deposits[payee].add(amount);
+
+        emit Deposited(payee, amount);
+    }
+
+      /**
+     * @dev Withdraw accumulated balance for a payee, forwarding all gas to the
+     * recipient.
+     *
+     * WARNING: Forwarding all gas opens the door to reentrancy vulnerabilities.
+     * Make sure you trust the recipient, or are either following the
+     * checks-effects-interactions pattern or using {ReentrancyGuard}.
+     *
+     * @param payee The address whose funds will be withdrawn and transferred to.
+     */
+    function withdraw(address payable payee) public virtual onlyOwner {
+        uint256 payment = _deposits[payee];
+
+        _deposits[payee] = 0;
+
+        payee.sendValue(payment);
+
+        emit Withdrawn(payee, payment);
+    }
+}
---
+++ utils/Address.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.6.0;
+
+library Address {
+    /**
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     */
+    function isContract(address account) internal view returns (bool) {
+        bytes32 codehash;
+        assembly { codehash := extcodehash(account) }
+        return (codehash != 0x0);
+    }
+
+    /**
+     * @dev Replacement for Solidity's `transfer`: sends `amount` pnch to
+     * `recipient`, forwarding all available gas and reverting on errors.
+     *
+    */
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call.value(amount)("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+}
